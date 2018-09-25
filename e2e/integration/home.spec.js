62f999a1ceb3dff36774b473731fda7fbd6b7dfd
Ada Solcan <asolcan@pivotal.io>
2018-09-25 17:33:46 +0100

    Fix linting errors and config in e2e tests
    Co-authored-by: Islam Gad <i_gad_@hotmail.com>
    Co-authored-by: Jonathan Sharpe <jsharpe@pivotal.io>
    Co-authored-by: Eslam Elsayed <eslam.elsayed.m@hotmail.com>

--- e2e/integration/home.spec.js
+++ e2e/integration/home.spec.js
@@ -1,6 +1,6 @@
 context('Home page', () => {
-    it('shows the React app', () => {
-        cy.visit('/');
-        cy.get('h1').should('have.text', 'Welcome to React');
-    });
+  it('shows the React app', () => {
+    cy.visit('/');
+    cy.get('h1').should('have.text', 'Welcome to React');
+  });
 });
