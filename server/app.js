c2b95854e4de8f47a003e23215606388dd4749a8
Eslam Elsayed <eslam.elsayed.m@hotmail.com>
2018-09-28 11:05:57 +0100

    Extract API routes from app.js
    Co-authored-by: Islam Gad <i_gad_@hotmail.com>
    Co-authored-by: Ada Solcan <asolcan@pivotal.io>
    Co-authored-by: Jonathan Sharpe <jsharpe@pivotal.io>

--- server/app.js
+++ server/app.js
@@ -1,20 +1,13 @@
 const express = require("express");
 const path = require("path");
 
+const router = require("./router");
+
 const app = express();
 const API_ROOT_PATH = "/api";
 app.use(express.json());
 
-app.post(`${API_ROOT_PATH}/login`, (req, res) => {
-  if (
-    req.body.username === "ellen@ip.org"
-    && req.body.password === "flamingo"
-  ) {
-    return res.sendStatus(200);
-  }
-
-  return res.sendStatus(401);
-});
+app.use(API_ROOT_PATH, router);
 
 app.use(express.static(path.join(__dirname, "static")));
 
---
+++ server/router.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = new express.Router();
+
+router.post(`/login`, (req, res) => {
+  if (
+    req.body.username === "ellen@ip.org" &&
+    req.body.password === "flamingo"
+  ) {
+    return res.sendStatus(200);
+  }
+
+  return res.sendStatus(401);
+});
+
+module.exports = router;
