c1ab253c415dbc9ab11f59c4d7f958d5854d3bf2
Eslam Elsayed <eslam.elsayed.m@hotmail.com>
2018-09-28 16:18:34 +0100

    Remove unused import
    Co-authored-by: Islam Gad <i_gad_@hotmail.com>
    Co-authored-by: Ada Solcan <asolcan@pivotal.io>
    Co-authored-by: Jonathan Sharpe <jsharpe@pivotal.io>

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import LoginPage from "./authentication/LoginPage";
 import HomePage from "./home/HomePage";
 
