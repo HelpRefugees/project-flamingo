676c2ad09fa94be37448f1a8193b2a3bf265e6c0
Ada Solcan <asolcan@pivotal.io>
2018-10-03 16:57:44 +0100

    Finalize styling for login page [#160751907 finishes]
    Co-authored-by: Islam Gad <i_gad_@hotmail.com>

--- client/src/authentication/LoginComponent.js
+++ client/src/authentication/LoginComponent.js
@@ -6,7 +6,8 @@ import {
   Paper,
   FormGroup,
   FormControl,
-  withStyles
+  withStyles,
+  FormHelperText
 } from "@material-ui/core";
 import { Redirect } from "react-router-dom";
 
@@ -16,8 +17,28 @@ const styles = themes => ({
   loginPaper: {
     padding: themes.spacing.unit * 4
   },
+  logo: {
+    paddingBottom: themes.spacing.unit * 3
+  },
   outerContainer: {
     height: "100vh"
+  },
+  formGroup: {
+    width: "100%"
+  },
+  fontFamily: {
+    fontFamily: "open Sans",
+    margin: themes.spacing.unit * 0.5,
+    fontWeight: "normal",
+    fontStyle: "normal",
+    fontStretch: "normal"
+  },
+  headerText: {
+    opacity: "0.87",
+    color: "#404040"
+  },
+  normalText: {
+    color: "#000000"
   }
 });
 
@@ -73,60 +94,79 @@ export class LoginComponent extends Component<Props, State> {
         className={classes.outerContainer}
       >
         <Grid container justify="center">
-          <Paper justify="center" className={classes.loginPaper}>
-            <Grid
-              container
-              justify="center"
-              direction="column"
-              alignItems="center"
-              spacing={8}
-            >
-              <img src="logo.png" alt="Help Refugees Logo" />
-              <h2>Welcome</h2>
-              <h5>Please enter your login details</h5>
-              <FormGroup>
-                <FormControl margin="normal" required>
-                  <TextField
-                    data-test-id="username-input"
-                    label="Email"
-                    type="email"
-                    variant="outlined"
-                    onChange={event =>
-                      this.changeCredentials("username", event)
-                    }
-                    value={this.state.credentials.username}
-                  />
-                </FormControl>
-                <FormControl margin="normal" required>
-                  <TextField
-                    data-test-id="password-input"
-                    label="Password"
-                    type="password"
-                    variant="outlined"
-                    onChange={event =>
-                      this.changeCredentials("password", event)
-                    }
-                    value={this.state.credentials.password}
-                  />
-                </FormControl>
-                <FormControl margin="normal">
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    data-test-id="login-button"
-                    onClick={() => this.login()}
-                  >
-                    Login
-                  </Button>
-                </FormControl>
-              </FormGroup>
-              {this.props.isAuthenticated === false && (
-                <Grid item>
-                  <p data-test-id="login-error">Invalid credentials</p>
-                </Grid>
-              )}
-            </Grid>
-          </Paper>
+          <Grid item xs={4}>
+            <Paper justify="center" className={classes.loginPaper}>
+              <Grid
+                container
+                justify="center"
+                direction="column"
+                alignItems="center"
+                spacing={8}
+              >
+                <img
+                  src="logo.png"
+                  alt="Help Refugees Logo"
+                  className={classes.logo}
+                />
+                <h2
+                  className={[classes.fontFamily, classes.headerText].join(" ")}
+                >
+                  Welcome
+                </h2>
+                <h5
+                  className={[classes.fontFamily, classes.normalText].join(" ")}
+                >
+                  Please enter your login details
+                </h5>
+                <FormGroup className={classes.formGroup}>
+                  <FormControl margin="normal" required>
+                    <TextField
+                      data-test-id="username-input"
+                      label="Email"
+                      type="email"
+                      fullWidth
+                      variant="outlined"
+                      onChange={event =>
+                        this.changeCredentials("username", event)
+                      }
+                      error={this.props.isAuthenticated === false}
+                      value={this.state.credentials.username}
+                    />
+                  </FormControl>
+                  <FormControl margin="normal" required>
+                    <TextField
+                      data-test-id="password-input"
+                      label="Password"
+                      type="password"
+                      variant="outlined"
+                      onChange={event =>
+                        this.changeCredentials("password", event)
+                      }
+                      error={this.props.isAuthenticated === false}
+                      value={this.state.credentials.password}
+                    />
+                  </FormControl>
+                  <FormControl>
+                    {this.props.isAuthenticated === false && (
+                      <FormHelperText error={true} data-test-id="login-error">
+                        Invalid credentials
+                      </FormHelperText>
+                    )}
+                  </FormControl>
+                  <FormControl margin="normal">
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      data-test-id="login-button"
+                      onClick={() => this.login()}
+                    >
+                      Login
+                    </Button>
+                  </FormControl>
+                </FormGroup>
+              </Grid>
+            </Paper>
+          </Grid>
         </Grid>
       </Grid>
     );
--- client/src/theme.js
+++ client/src/theme.js
@@ -2,7 +2,7 @@ import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 export default createMuiTheme({
   palette: {
-    primary: { main: "#4bc6b9" },
+    primary: { main: "#00857B" },
     background: { default: "#f4f4f4", paper: "#ffffff" }
   }
 });
