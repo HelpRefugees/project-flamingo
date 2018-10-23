4a3639ae840f93e07334906c0bc09c86da503152
Eslam Elsayed <eslam.elsayed.m@hotmail.com>
2018-10-23 10:14:06 +0100

    Add auth debugging and simplify user serialisation [#161316095]
    Co-authored-by: Jonathan Sharpe <jsharpe@pivotal.io>

--- server/auth.js
+++ server/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const debug = require("debug")("auth");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 
@@ -7,13 +8,15 @@ module.exports = {
     const collection = "users";
 
     passport.serializeUser((user, done) => {
-      done(null, user.username);
+      const { username, name, role } = user;
+      debug("serialising", user.username);
+      done(null, JSON.stringify({ username, name, role }));
     });
 
-    passport.deserializeUser((username, done) => {
-      db.collection(collection).findOne({ username }, (err, user) => {
-        done(err, user);
-      });
+    passport.deserializeUser((payload, done) => {
+      const user = JSON.parse(payload);
+      debug("deserialising", user.username);
+      done(null, user);
     });
 
     app.use(passport.initialize());
@@ -21,6 +24,7 @@ module.exports = {
 
     passport.use(
       new LocalStrategy((username, password, done) => {
+        debug("logging in", username);
         db.collection(collection).findOne({ username }, (err, user) => {
           if (user && bcrypt.compareSync(password, user.password)) {
             const { username, name, role } = user;
@@ -32,6 +36,7 @@ module.exports = {
     );
   },
   ensureLoggedIn: (req, res, next) => {
+    debug("ensuring", req.user ? req.user.username : "<unknown>");
     if (!req.user) {
       return res.sendStatus(401);
     }
