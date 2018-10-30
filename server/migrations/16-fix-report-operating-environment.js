952de7ef34b88e530a805c0f02904b4c74dce929
Eslam ElSayed <Eslam@E0414603.internal.vodafone.com>
2018-10-30 18:18:07 +0200

    Fix operating environment field empty object

---
+++ server/migrations/16-fix-report-operating-environment.js
@@ -0,0 +1,29 @@
+"use strict";
+
+module.exports.id = "fix-report-operating-environment";
+
+module.exports.up = function (done) {
+  this.db
+    .collection("reports")
+    .updateMany(
+      { operatingEnvironment: {} },
+      {
+        $set: {
+          operatingEnvironment: ""
+        }
+      }
+    )
+    .then(() => done());
+};
+
+module.exports.down = function (done) {
+  this.db
+    .collection("reports")
+    .updateMany(
+      { operatingEnvironment: "" },
+      {
+        $set: { operatingEnvironment: {} }
+      }
+    )
+    .then(() => done());
+};
