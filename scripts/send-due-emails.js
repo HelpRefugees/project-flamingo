5b874130604fdbbd7bd28e6c047af3164593c493
Eslam ElSayed <Eslam@E0414603.internal.vodafone.com>
2018-12-18 08:38:11 +0200

    refactoring send-due-mails

--- scripts/send-due-emails.js
+++ scripts/send-due-emails.js
@@ -9,15 +9,6 @@ const { getDatabaseUrl } = require("./utils");
 
 const reportFinder = new ReportFinder(getDatabaseUrl());
 
-// const grantName = { grant: "Eslam Grant" };
-// emailSender.send(
-//   "report-due",
-//   [{ owner: "eslam.elsayed-ibrahim@vodafone.com" }],
-//   grantName
-// );
-// sendIT = (task, recep, obj) => emailSender.send(task, recep, obj);
-// sendIT("report-due", ["eslam.elsayed-ibrahim@vodafone.com"], grantName);
-
 reportFinder
   .due()
   .then(reports =>
@@ -27,7 +18,6 @@ reportFinder
     Promise.all(
       modifiedReports.map(report => {
         console.log("emailed first", report.owner, "****", report.grant);
-        // const grantName = { grant: report.grant };
         return emailSender.send("report-due", [report.owner], {
           grant: report.grant
         });
@@ -41,18 +31,3 @@ reportFinder
     console.error(err);
     process.exit(1);
   });
-
-// reportFinder
-//   .due()
-//   .then(reports => reports.map(report => report.owner))
-//   .then(recipients =>
-//     emailSender.send("report-due", recipients).then(() => recipients)
-//   )
-//   .then(recipients => {
-//     console.log("emailed", recipients);
-//     process.exit(0);
-//   })
-//   .catch(err => {
-//     console.error(err);
-//     process.exit(1);
-//   });
