f2b5ad43b6387e0f38f2b1155743c508413855b0
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2020-02-03 21:13:40 +0000

    Refactor coverage script and remove client from reporting
    For some reason, the client coverage files are broken...

--- coverage.sh
+++ coverage.sh
@@ -1,16 +1,25 @@
 #! /usr/bin/env bash
-set -e -x
+set -euo pipefail
 
-./cc-test-reporter format-coverage -o "./coverage/cc.server.json"
+HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPORTER="$HERE/cc-test-reporter"
 
-for DIR in client scripts
-do
-    pushd $DIR
-        ../cc-test-reporter format-coverage -o "../coverage/cc.$DIR.json" --add-prefix $DIR
-    popd
-done
+function format_coverage() {
+    "$REPORTER" format-coverage \
+        "$1/coverage/lcov.info" \
+        --input-type lcov \
+        --output "$HERE/coverage/cc.${2:-server}.json"
+}
 
-PATTERN=coverage/cc.*.json
-FILES=($PATTERN)
+if [[ "${TRAVIS_TEST_RESULT:-0}" == 0 ]]; then
+    format_coverage "$HERE"
+    # client coverage includes C:/Users/Dolan/Documents/docx for some reason...
+    # format_coverage "$HERE/client" 'client'
+    format_coverage "$HERE/scripts" 'scripts'
 
-./cc-test-reporter sum-coverage $PATTERN -p ${#FILES[@]}
+    PATTERN=coverage/cc.*.json
+    FILES=($PATTERN)
+
+    "$REPORTER" sum-coverage $PATTERN -p ${#FILES[@]}
+    "$REPORTER" upload-coverage
+fi
